3bb065f636e845f5e403e2c645ca156029948f87
sheedhet <hetsheed@gmail.com>
2015-04-19 18:18:39 -0600

    Add equivalence testing via json to Play class

--- app/models/play.rb
+++ app/models/play.rb
@@ -6,6 +6,16 @@ class Play
     @player = @game.players[position]
   end
 
+  def ==(other_game)
+    other_game.class == self.class && other_game.as_json == as_json
+  end
+
+  alias_method :eql?, :==
+
+  def hash
+    as_json.hash
+  end
+
   def as_json
     { action: @action,
       game: @game,
