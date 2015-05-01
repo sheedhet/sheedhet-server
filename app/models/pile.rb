5c557ed15a4b108254b44ca9374c555c1d459a26
sheedhet <hetsheed@gmail.com>
2015-04-30 21:57:48 -0600

    Create Pile class for some extra Array convenience

---
+++ app/models/pile.rb
@@ -0,0 +1,11 @@
+class Pile < Array
+  def add(card)
+    @cards << card
+  end
+
+  def remove(card)
+    index = @cards.index(card)
+    raise ArgumentError, "Card #{card.as_json} not found" if index.nil?
+    @cards.slice! index
+  end
+end
