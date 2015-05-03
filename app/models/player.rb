8c61268c3d473de4f075879d8e835ad361bb6797
sheedhet <hetsheed@gmail.com>
2015-05-03 14:04:31 -0600

    Clean up old code from Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -22,34 +22,6 @@ class Player
     }
   end
 
-  # def remove_from_hand(card)
-  #   remove_from(card: card, target: :in_hand)
-  # end
-
-  # def remove_from_face_up(card)
-  #   remove_from(card: card, target: :face_up)
-  # end
-
-  # def remove_from_face_down(card)
-  #   remove_from(card: card, target: :face_down)
-  # end
-
-  # def add_to_hand(card)
-  #   add_to(card: card, target: :in_hand)
-  # end
-
-  # def add_to_face_up(card)
-  #   add_to(card: card, target: :face_up)
-  # end
-
-  # def remove_from(card:, target:)
-  #   cards[target].remove card
-  # end
-
-  # def add_to(card:, target:)
-  #   cards[target] << card
-  # end
-
   def random_name
     [ 'Ted',
       'Bill',
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,32 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Player do
-  let(:game) { GameFactory.new.build }
-  let(:player) { game.players.sample }
-  let(:target) { Player::PILES.sample }
-  let(:card) { player.cards[target].sample }
-  subject(:cards) { player.cards[target] }
 
-  # describe '#remove_from' do
-  #   context 'card exists in target' do
-  #     let!(:removed) { player.remove_from(card: card, target: target) }
-  #     it { expect(removed).to be card }
-  #     it { is_expected.not_to include(card) }
-  #   end
-
-  #   context 'card does not exist in target' do
-  #     let(:card) { Card.new(suit: 's', face: '0') }
-  #     it 'should raise error' do
-  #       expect do
-  #         player.remove_from(card: card, target: target)
-  #       end.to raise_error
-  #     end
-  #   end
-  # end
-
-  # describe '#add_to' do
-  #   let(:card) { Card.new(suit: 's', face: '0') }
-  #   before { player.add_to(card: card, target: target) }
-  #   it {is_expected.to include card }
-  # end
 end
