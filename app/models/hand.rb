b01fc840c6175c4cb7921acd6cbe4aef7fd2f80a
sheedhet <hetsheed@gmail.com>
2015-06-29 22:10:36 -0600

    Major cleanup with help from rubocop
    - token documentation
    - class constant dependant on external class
    - add depedency injection
    - dont try to return from constructor
    - added #flatten method

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -1,39 +1,29 @@
+# Handles a player's hand of cards and their order of importance
+#
 class Hand < Hash
   include JsonEquivalence
 
   PILES = [:in_hand, :face_up, :face_down]
-  # DEFAULT = PILES.map{ |pile| [pile, Pile.new] }.to_h
-  DEFAULT = {
-    in_hand: Pile.new,
-    face_up: Pile.new,
-    face_down: Pile.new
-  }
 
-  # seems like we need a Hand class for handling groups of piles and their
-  # order of operations, get on it.
-  def initialize(existing = {})
-    # require 'pry'
-    # binding.pry
-    thing = Hand::DEFAULT.merge existing
-    puts "returning: #{thing}"
-    thing
+  def initialize(existing = {}, storage_type = Pile, pile_names = PILES)
+    empty_hand = pile_names.map { |pile| [pile, storage_type.new] }.to_h
+    merge!(empty_hand.merge existing)
   end
 
   def get_playable(operator:, value:, from_piles: keys.each)
-    # require 'pry'
-    # binding.pry
     pile = from_piles.next
     result = Hand.new
     result[pile] = self[pile].get(operator, value)
     empties_pile = result[pile].size == self[pile].size
     if pile.empty? || (empties_pile && result[pile].all_same?)
-      Hand.new(result.merge get_playable(
+      result.merge! get_playable(
         from_piles: from_piles,
         operator: operator,
-        value: value
-      ))
+        value: value)
     end
-    Hand.new result
   end
 
+  def flatten
+    delete_if { |_, pile| pile.empty? }
+  end
 end
