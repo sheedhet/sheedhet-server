4821c1b37f2825bd9e1ecd6ae4f56b5ffde08402
sheedhet <hetsheed@gmail.com>
2015-06-29 22:12:35 -0600

    Rubocop cleanup
    - token docs
    - fudge method lines with less spacing
    - fail instead of raise?

--- app/services/turn.rb
+++ app/services/turn.rb
@@ -1,14 +1,11 @@
+# Superclass for all Turn types
+#
 class Turn
   include JsonEquivalence
 
   attr_reader :position
 
-  def self.build(
-    action:,
-    game:,
-    play_cards: Hand.new,
-    position:
-  )
+  def self.build(action:, game:, play_cards: Hand.new, position:)
     klass = action.to_s.camelize.constantize
     klass.new(
       action: action,
@@ -18,17 +15,12 @@ class Turn
     )
   end
 
-  def initialize(
-    action:,
-    game:,
-    play_cards:,
-    position:
-  )
-  @action = action
-  @game = game  # pull game from db
-  @play_cards = play_cards
-  @position = position
-  @player = @game.players[position]
+  def initialize(action:, game:, play_cards:, position:)
+    @action = action
+    @game = game  # pull game from db
+    @play_cards = play_cards
+    @position = position
+    @player = @game.players[position]
   end
 
   def as_json
@@ -44,6 +36,6 @@ class Turn
   end
 
   def execute
-    raise ArgumentError, 'Not a valid Turn' unless valid?
+    fail ArgumentError, 'Not a valid Turn' unless valid?
   end
 end
