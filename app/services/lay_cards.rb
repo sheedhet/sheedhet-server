ef0289f3e861f88edda3ea6ba336dc8a82ef7fd2
sheedhet <hetsheed@gmail.com>
2015-07-01 23:49:38 -0600

    Rubocop cleanup for LayCards

--- app/services/lay_cards.rb
+++ app/services/lay_cards.rb
@@ -1,10 +1,12 @@
+# Service facilitates a player laying card(s) from hand to play pile
 class LayCards < Turn
   ACTION = :lay_cards
-  VALID_FACES = ['a', '4', '5', '6', '9', 'j', 'q', 'k']
+  VALID_FACES = %w(a, 4, 5, 6, 9, j, q, k)
 
   def execute
     # require 'pry'
     # binding.pry
+
     @play_cards.each do |pile|
       @play_cards[pile].each do |card|
         @game.play_pile.add @player.cards[pile].remove(card)
