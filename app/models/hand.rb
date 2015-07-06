adc18ba060a62310934b13e167bec13ec49e5a70
sheedhet <hetsheed@gmail.com>
2015-07-05 18:02:53 -0600

    Implement Hand#get_playable

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -5,21 +5,27 @@ class Hand < Hash
 
   PILES = [:in_hand, :face_up, :face_down]
 
+  # def self.from_json(json_hand)
+  # json_hand.each do ||
+  # end
+  # end
+
   def initialize(existing = {}, storage_type = Pile, pile_names = PILES)
     empty_hand = pile_names.map { |pile| [pile, storage_type.new] }.to_h
     merge!(empty_hand.merge existing)
   end
 
   def get_playable(operator:, value:)
-    all_by_operator = inject(Hand.new) do |response, (name, next_pile)|
-      response.merge "#{name}": next_pile.get(operator, value)
+    all_by_operator = inject(Hand.new) do |playable, (name, next_pile)|
+      playable.merge "#{name}": next_pile.get(operator, value)
     end
     all_by_operator.trim_unplayable
   end
 
   def trim_unplayable
-    each do |(pile_name, pile)|
-
+    inject(Hand.new) do |trimmed, (pile_name, pile)|
+      trimmed.merge!(pile_name => pile)
+      break trimmed unless pile.all_same?
     end
   end
 
