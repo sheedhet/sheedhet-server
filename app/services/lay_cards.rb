c48ad8f86f49d69b1945f156eb3c5402495e942d
sheedhet <hetsheed@gmail.com>
2015-07-05 18:03:40 -0600

    Hash#each returns both a key and value

--- app/services/lay_cards.rb
+++ app/services/lay_cards.rb
@@ -4,12 +4,9 @@ class LayCards < Turn
   VALID_FACES = %w(a, 4, 5, 6, 9, j, q, k)
 
   def execute
-    # require 'pry'
-    # binding.pry
-
-    @play_cards.each do |pile|
-      @play_cards[pile].each do |card|
-        @game.play_pile.add @player.cards[pile].remove(card)
+    @play_cards.each do |pile_name, pile|
+      pile.each do |card|
+        @game.play_pile.add @player.cards[pile_name].remove(card)
       end
     end
   end
