0ea2c13aad2cfbc2cce17168e1f6925d26308c45
sheedhet <hetsheed@gmail.com>
2015-07-19 21:46:04 -0600

    Get Pile class off the Array inheritance

--- app/models/pile.rb
+++ app/models/pile.rb
@@ -1,21 +1,34 @@
 # class responsible for individual piles of cards
 #
-class Pile < Array
+class Pile
   include JsonEquivalence
+  # attr_reader :data
 
-  def self.from_json(json_pile)
-    Pile.new(json_pile.map { |card| Card.from_json(card) })
+  def self.from_json(json_pile, content = Card)
+    new(json_pile.map { |json| content.from_json(json) })
+  end
+
+  def initialize(existing = [], content = Card)
+    @data = Array.new existing.map { |object| content.from_json object }
+  end
+
+  def as_json
+    @data.as_json
+  end
+
+  def inspect
+    "Pile:#{as_json}"
   end
 
   def add(card)
-    self << card
-    self
+    @data << card
+    @data
   end
 
   def remove(card)
-    index = self.index(card)
+    index = @data.index(card)
     fail ArgumentError, "Card #{card.as_json} not found" if index.nil?
-    self.slice! index
+    @data.slice! index
   end
 
   def all_same?
@@ -23,19 +36,15 @@ class Pile < Array
   end
 
   def by_values
-    map(&:value)
+    @data.map(&:value)
   end
 
   def get(operator, value)
     return new unless [:==, :>=, :<=].include? operator
-    Pile.new select { |card| card.value.public_send(operator, value) }
+    Pile.new @data.select { |card| card.value.public_send(operator, value) }
   end
 
   def get_all(face)
-    Pile.new select { |card| card.face == face }
+    Pile.new @data.select { |card| card.face == face }
   end
-
-  # def uniq_face
-  #   new self.uniq(&:face)
-  # end
 end
