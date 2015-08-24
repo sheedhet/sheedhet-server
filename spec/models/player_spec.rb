f30ab46695b7f9ef5dddddee277d152bdd8c525e
sheedhet <hetsheed@gmail.com>
2015-08-23 22:00:20 -0600

    Update player_spec
    - use mocks instead of dependencies
    - listen for signals

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Player do
-  let(:simple_hand) do
-    Hand.new({ in_hand: Pile.from_json(['4h', '5h', '6h', '7h', '8h']) })
-  end
-  let(:player) { Player.new(cards: simple_hand) }
+  let(:hand) { double(Hand) }
 
-  # before { require 'pry'; binding.pry }
+  let(:params) { { operator: :>=, value: 6 } }
+  let(:player) { Player.new(cards: hand) }
+
   describe '#get_playable' do
-    context 'getting values greater than/equal to' do
-      subject { player.get_playable(operator: :>=, value: 6) }
+    it 'sends #get_playable to cards' do
+      expect(hand).to receive(:get_playable).with(params)
+      player.get_playable(params)
+    end
+  end
 
-      it "returns only cards with values greater than or equal to" do
-        expect(subject[:in_hand].count{ |card| card.value < 6 }).to eq(0)
-      end
+  describe 'lowest_card' do
+    it 'sends #min to cards' do
+      expect(hand).to receive(:lowest_card)
+      player.lowest_card
     end
   end
 end
