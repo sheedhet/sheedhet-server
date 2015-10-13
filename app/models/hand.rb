83085eeb120b36b9c44e7ebc09d9b25144ee070a
sheedhet <hetsheed@gmail.com>
2015-10-12 21:46:49 -0600

    Eliminate inheritance, use Direction, cleanup
    - Stop inheriting from Hash, use an internal @data
    - Direction allows forwarding and casting to self.class
    - add #initialize
    - add #as_json
    - add #add_to and #remove_from

--- app/models/hand.rb
+++ app/models/hand.rb
@@ -1,30 +1,52 @@
 # Handles a player's hand of cards and their order of importance
 #
-class Hand < Hash
+class Hand
   include JsonEquivalence
+  extend Direction
 
-  PILE_NAMES = %i(in_hand face_up face_down)
+  CONTAINER_NAMES = %i(in_hand face_up face_down)
 
-  # def self.from_json(json_hand)
-  # json_hand.each do ||
-  # end
-  # end
+  DELEGATE_HASH_COMMANDS = %i(merge!)
+  DELEGATE_HASH_QUERIES = %i([] each)
 
-  def initialize(existing = {}, storage_type = Pile, pile_names = PILE_NAMES)
-    empty_hand = pile_names.map { |pile| [pile, storage_type.new] }.to_h
-    merge! empty_hand.merge existing
+  attr_reader :container_names
+
+  command DELEGATE_HASH_COMMANDS => :@data
+  query DELEGATE_HASH_QUERIES => :@data
+
+  def initialize(
+    existing = {},
+    container = Pile,
+    container_names = CONTAINER_NAMES
+  )
+    @container = container
+    @container_names = container_names
+    empty_hand = container_names.map { |pile| [pile, container.new] }.to_h
+    @data = empty_hand.merge(existing)
+  end
+
+  def as_json
+    @data.map { |name, pile| [name, pile.as_json] }.to_h
   end
 
   def get_playable(operator:, value:)
-    all_by_operator = each_with_object(Hand.new) do |(name, pile), playable|
-      playable.merge! "#{name}": pile.get(operator, value)
+    new_hand = Hand.new(
+      container: @container,
+      container_names: @container_names
+    )
+    all_by_operator = @data.each_with_object(new_hand) do |(name, pile), result|
+      result.merge!("#{name}": pile.get(operator, value))
     end
     all_by_operator.trim_unplayable
   end
 
   def trim_unplayable
-    each_with_object(Hand.new) do |(pile_name, pile), trimmed|
-      trimmed.merge!(pile_name => pile)
+    new_hand = Hand.new(
+      container: @container,
+      container_names: @container_names
+    )
+    @data.each_with_object(new_hand) do |(pile_name, pile), result|
+      result.merge!(pile_name => pile)
       break unless pile.all_same?
     end
   end
@@ -33,7 +55,11 @@ class Hand < Hash
     self[:in_hand].min
   end
 
-  def flatten
-    reject! { |_, pile| pile.empty? }
+  def add_to(target:, subject:)
+    @data[target].add(subject)
+  end
+
+  def remove_from(target:, subject:)
+    @data[target].remove(subject)
   end
 end
