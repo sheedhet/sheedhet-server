afc9c8ced1188ba5bc07d8b9ff51aa1ce3789883
sheedhet <hetsheed@gmail.com>
2016-02-09 21:04:56 -0700

    Update Card for new style guide rules

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,8 +3,8 @@
 class Card
   include JsonEquivalence
 
-  SUITS = %w(c d h s)
-  FACES = %w(a 2 3 4 5 6 7 8 9 10 j q k)
+  SUITS = %w(c d h s).freeze
+  FACES = %w(a 2 3 4 5 6 7 8 9 10 j q k).freeze
   VALUES = {
     'j' => 11,
     'q' => 12,
@@ -13,7 +13,7 @@ class Card
     '2' => 15,
     '3' => 15,
     '10' => 15
-  }
+  }.freeze
 
   attr_reader :suit, :face
 
@@ -27,15 +27,15 @@ class Card
   end
 
   def initialize(suit: SUITS.sample, face: FACES.sample)
-    fail ArgumentError, "Invalid suit: #{suit}" unless SUITS.include?(suit)
-    fail ArgumentError, "Invalid face: #{face}" unless FACES.include?(face)
+    raise ArgumentError, "Invalid suit: #{suit}" unless SUITS.include?(suit)
+    raise ArgumentError, "Invalid face: #{face}" unless FACES.include?(face)
     @suit = suit
     @face = face
   end
 
   def <=>(other)
     is_card = other.class == Card
-    fail ArgumentError, "Can't compare Card to non-Card" unless is_card
+    raise ArgumentError, "Can't compare Card to non-Card" unless is_card
     self_as_values = [value, face, suit]
     other_as_values = [other.value, other.face, other.suit]
     self_as_values <=> other_as_values
