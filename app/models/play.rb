00b42ca49b4aa3589c2c794592b0b2e1efaa195a
sheedhet <hetsheed@gmail.com>
2016-05-03 23:00:17 -0600

    Add Play class to represent a turn/play

---
+++ app/models/play.rb
@@ -0,0 +1,28 @@
+# represents a possible game turn
+class Play
+  include JsonEquivalence
+
+  attr_reader :player, :hand
+
+  def initialize(player:, hand:)
+    @player = player
+    @hand = hand
+  end
+
+  def value
+    first_card.value
+  end
+
+  def face
+    first_card.face
+  end
+
+  def non_empty_piles
+    hand.compact
+  end
+
+  def first_card
+    _pile_name, cards = non_empty_piles.first
+    cards.first
+  end
+end
