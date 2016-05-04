8ce024205e3dd38d3931b253dfe5616f434cba97
sheedhet <hetsheed@gmail.com>
2016-05-03 22:57:46 -0600

    GameFactory updates
    - enable CardDealer
    - #freeze constants
    - doesn't need Game reference
    - Updated for new Game interface

--- app/services/game_factory.rb
+++ app/services/game_factory.rb
@@ -5,14 +5,14 @@ class GameFactory
     player_class: Player,
     game_class: Game,
     # card_class: Card,
-    # dealer_class: CardDealer
-  }
+    dealer_class: CardDealer
+  }.freeze
 
   DEFAULT_OPTIONS = {
     hand_size: 4,
     num_decks: 1,
     num_players: 3
-  }
+  }.freeze
 
   def self.build(state: {})
     factory = new(state: state)
@@ -29,41 +29,38 @@ class GameFactory
     @num_players = options[:num_players]
     @state = state
     define_classes(classes)
-    @game = @game_class.new
   end
 
   def define_classes(param_classes)
     all_classes = DEFAULT_CLASSES.merge param_classes
     @player_class = all_classes[:player_class]
     @game_class = all_classes[:game_class]
+    @dealer = all_classes[:dealer_class]
   end
 
   def reconstitute_from_state
-    fail "You haven't written reconstitute_from_state yet"
+    raise "You haven't written reconstitute_from_state yet"
   end
 
   def create_new_game
-    @players = create_players
-    # @game_cards = create_game_cards
-    # deal_new_game
-    @game_class.new.tap do |game|
-      # game.draw_pile = @game_cards
-      game.hand_size = @hand_size
-      game.players = @players
-    end
+    deck = create_cards
+    game = @game_class.new(deck, @hand_size)
+    players = create_players
+    players.each { |p| game.add_player(p) }
+    @dealer.deal(game)
+    game
   end
 
-  def deck=(deck)
-    @draw_pile = deck
+  def create_cards(deck_factory = DeckFactory)
+    deck_factory.build(@num_decks)
   end
 
   private
 
   def create_players(existing_players: [])
-    @num_players.times do |i|
+    Array.new(@num_players) do |i|
       new_player_details = existing_players.fetch(i, position: i)
-      new_player = @player_class.new(new_player_details)
-      @game.add_player(new_player)
+      @player_class.new(new_player_details)
     end
   end
 end
