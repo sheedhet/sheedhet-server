f5c8534819b95216ab80b4df9701916870e7ee55
sheedhet <hetsheed@gmail.com>
2016-05-10 22:29:59 -0600

    WIP Completing Pile spec

--- spec/models/pile_spec.rb
+++ spec/models/pile_spec.rb
@@ -4,12 +4,34 @@ require_relative '../../app/modules/direction.rb'
 require_relative '../../app/models/pile.rb'
 
 RSpec.describe Pile do
-  let(:empty_pile) { [] }
-  let(:card) { double('Card') }
-  let(:pile_with_card) { empty_pile.push(card) }
-  subject(:pile) { Pile.new(existing, card.class) }
+  let(:content) { double(Card) }
+  describe '.from_json' do
+    let(:card1_face) { 'a' }
+    let(:card1_suit) { 's' }
+    let(:card2_face) { '9' }
+    let(:card2_suit) { 'd' }
+    let(:card1_json) { "#{card1_face}#{card1_suit}" }
+    let(:card2_json) { "#{card2_face}#{card2_suit}" }
+    let(:content) { double(Card) }
+    subject { Pile.from_json("[#{card1_json}, #{card2_json}]", content) }
+
+    # !!!!!!!!!!!!!!!!!!!!
+    # learn some more about mocking classes in tests!!!
+    # !!!!!!!!!!!!!!!!!!!!
+
+    it do
+      is_expected.to contain_exactly(
+        Card.new(suit: card1_suit, face: card1_face),
+        Card.new(suit: card2_suit, face: card2_face)
+      )
+    end
+  end
 
   describe '#add' do
+    let(:empty_pile) { [] }
+    let(:card) { double('Card') }
+    let(:pile_with_card) { empty_pile.push(card) }
+    subject(:pile) { Pile.new(existing, card.class) }
     context 'adding a card' do
       let(:existing) { empty_pile }
       before { pile.add card }
