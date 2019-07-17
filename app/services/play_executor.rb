e77ba46a80ad474cef780c72146f118b44dc5443
sheedhet <hetsheed@gmail.com>
2019-07-16 19:20:59 -0600

    Updates for playing 10s

--- app/services/play_executor.rb
+++ app/services/play_executor.rb
@@ -9,41 +9,25 @@ class PlayExecutor
   end
 
   def valid?
-    if play.destination == :swap
-      valid_swap?
-    else
-      game.valid_plays.any? { |valid_play| valid_play.contains?(play) }
-      # #############################################################
-      # I think with the inclusion of a :contains? chain of methods,
-      # we're good to go with the valid play detection and perhaps
-      # this is good enough to get us on to whatever the next step
-      # is. Unfortunately it looks like our docker aint working no
-      # more so maybe now's the time to do a full migration to the
-      # latest and greatest platform tech?!
-      # - new rails
-      # - new docker bs
-      # - new webpacker bs
-      # - new etc bs
-      # #############################################################
-    end
+    game.valid_plays.any? { |valid_play| valid_play.contains?(play) }
   end
 
-  def valid_swap?
-    game.valid_plays.any? do |valid_play|
-      valid_play.position == play.position
-    end
-    [:in_hand, :face_up].all? do |pile_name|
-      player.hand[pile_name].contains?(play.hand[pile_name])
-    end
-  end
+  # def valid_swap?
+  #   game.valid_plays.any? do |valid_play|
+  #     valid_play.position == play.position
+  #   end
+  #   [:in_hand, :face_up].all? do |pile_name|
+  #     sult = player.cards[pile_name].contains?(play.hand[pile_name])
+  #   end
+  # end
 
   def execute!
     raise InvalidPlayError unless valid?
     send_cards_to_destination
     refill_hand
-    clear_play_pile if four_of_a_kind_in_play?
+    clear_play_pile if four_of_a_kind_in_play? || ten_played?
+    game.history.push(play: play) #, state: game.as_json)
     game.update_valid_plays!
-    game.history.push(play: play, state: game.as_json)
     game
   end
 
@@ -57,8 +41,11 @@ class PlayExecutor
     case play.destination
     when :play_pile
       play_from_player
-    when :discard_pile
-      clear_play_pile
+    when :in_hand
+      pickup_play_pile
+    # when :discard_pile
+      # clear_play_pile
+      # shouldn't this be where players pick up the play pile??
     when :swap
       perform_swap
     end
@@ -68,14 +55,14 @@ class PlayExecutor
     # mark equal number of cards in hand and face up, swap their positions
     from_face_up = play.hand[:face_up]
     from_in_hand = play.hand[:in_hand]
-    player.hand[:face_up] -= from_face_up
-    player.hand[:face_up] += from_in_hand
-    player.hand[:in_hand] -= from_in_hand
-    player.hand[:in_hand] += from_face_up
+    from_face_up.each { |c| player.cards.remove_from(target: :face_up, subject: c) }
+    from_in_hand.each { |c| player.cards.add_to(target: :face_up, subject: c) }
+    from_in_hand.each { |c| player.cards.remove_from(target: :in_hand, subject: c) }
+    from_face_up.each { |c| player.cards.add_to(target: :in_hand, subject: c) }
   end
 
   def pickup_play_pile
-    player.hand[:in_hand] = player.hand[:in_hand] + game.play_pile
+    game.play_pile.each { |c| player.cards.add_to(target: :in_hand, subject: c) }
     game.play_pile = Pile.new
   end
 
@@ -85,15 +72,19 @@ class PlayExecutor
   end
 
   def refill_hand
-    while player_needs_cards? && draw_pile_not_empty?
+    while player_in_hand_empty? && draw_pile_not_empty?
       player.add_to(target: :in_hand, subject: game.draw_pile.pop)
     end
   end
 
   def four_of_a_kind_in_play?
     return false if game.play_pile.size < 4
-    fourth_card = game.play_pile[-4]
-    game.play_pile.last(3).all? { |c| c.face == fourth_card.face }
+    last4 = game.play_pile.last(4)
+    last4.all? { |c| c.face == last4[0].face }
+  end
+
+  def ten_played?
+    !game.play_pile.empty? && game.play_pile.last.face == :'10'
   end
 
   def clear_play_pile
@@ -101,8 +92,8 @@ class PlayExecutor
     game.play_pile = Pile.new
   end
 
-  def player_needs_cards?
-    player.cards[:in_hand].size < game.hand_size
+  def player_in_hand_empty?
+    player.cards[:in_hand] && player.cards[:in_hand].size < game.hand_size
   end
 
   def draw_pile_not_empty?
