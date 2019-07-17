cb9ec9a982ccd8b547330d38a832ddf66eb7f853
sheedhet <hetsheed@gmail.com>
2019-07-16 19:26:18 -0600

    simplify card, move skew angle up to Pile

--- app/webpacker/card.js
+++ app/webpacker/card.js
@@ -1,72 +1,50 @@
 import React from 'react'
 
-export default function Card(props) {
-  const suit_lookup = {
-    'd': 'diamonds',
-    'h': 'hearts',
-    's': 'spades',
-    'c': 'clubs',
+const Card = (props) => {
+  const suit_lookup = (suit_string) => {
+    return {
+      'd': 'diamonds',
+      'h': 'hearts',
+      's': 'spades',
+      'c': 'clubs',
+    }[suit_string]
   }
 
-  const rank_lookup = {
-    'a': 'rank1',
-    '2': 'rank2',
-    '3': 'rank3',
-    '4': 'rank4',
-    '5': 'rank5',
-    '6': 'rank6',
-    '7': 'rank7',
-    '8': 'rank8',
-    '9': 'rank9',
-    '10': 'rank10',
-    'j': 'rank11',
-    'q': 'rank12',
-    'k': 'rank13',
+  const rank_lookup = (rank_string) => {
+    return {
+      'a': 'rank1',
+      '2': 'rank2',
+      '3': 'rank3',
+      '4': 'rank4',
+      '5': 'rank5',
+      '6': 'rank6',
+      '7': 'rank7',
+      '8': 'rank8',
+      '9': 'rank9',
+      '10': 'rank10',
+      'j': 'rank11',
+      'q': 'rank12',
+      'k': 'rank13',
+    }[rank_string]
   }
 
-  const random_angle = (min, max) => {
-    let minimum = Math.ceil(min)
-    let maximum = Math.floor(max)
-    //The maximum is exclusive and the minimum is inclusive
-    let angle = Math.floor(Math.random() * (maximum - minimum)) + minimum
-    return angle
-  }
-
-  const random_skew_angle = () => {
-    return random_angle(-30.0, 30.0) / 15.0
-  }
-
-  const card_style = {
-    transform: `rotate(${random_skew_angle()}deg)`
-  }
-
-  const suit = suit_lookup[props.cardString.slice(-1)]
-  const rank = rank_lookup[props.cardString.slice(0, -1)]
+  const suit = suit_lookup(props.cardString.slice(-1))
+  const rank = rank_lookup(props.cardString.slice(0, -1))
   const face_up = props.cardString == 'xx' ? 'back' : 'face'
-  const css_classes = ['card', suit, rank, props.playCssClasses].join(' ')
-  const has_plays = (props.playCssClasses.match(/\bplay\w+/) || []).length
-  const input_id = `${props.pileName}-${props.cardString}`
+  const css_classes = ['card', suit, rank].join(' ')
+  const card_style = { transform: `rotate(${props.skew_angle}deg)` }
 
-  return (
-    <div
+  return(
+    <label
+      className={css_classes}
       style={card_style}
-      data-index={props.index}
       data-string={props.cardString}
+      data-index={props.index}
+      htmlFor={props.index}
     >
-      <input
-        className="card-selector"
-        type="checkbox"
-        disabled={has_plays ? '' : ' disabled'}
-        id={input_id}
-      />
-      <label
-        className={css_classes}
-        htmlFor={input_id}
-        onMouseOver={props.mouseOverCardHandler}
-        onMouseOut={props.mouseOutCardHandler}
-      >
-        <div className={face_up}></div>
-      </label>
-    </div>
+      <div className={face_up}></div>
+    </label>
   )
 }
+
+export default Card
