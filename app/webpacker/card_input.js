f04fed429c56997f58cb700a550878aaad857c58
sheedhet <hetsheed@gmail.com>
2019-07-26 19:05:22 -0600

    CSS classes use hyphens not underscores

--- app/webpacker/card_input.js
+++ app/webpacker/card_input.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CardInput = (props) => {
   return (
     <input
-      className={`card_input`}
+      className="card-input"
       type="checkbox"
       id={props.id}
       onChange={props.onChangeHandler}
